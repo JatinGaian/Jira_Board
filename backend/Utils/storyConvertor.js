c146c6d3bdc4a4df50d23a64011234bf437a4305
jatin-gaian <jatin.s@gaiansolutions.com>
2024-08-28 17:47:55 +0530

    story points estimate added customfield_10026

--- backend/Utils/storyConvertor.js
+++ backend/Utils/storyConvertor.js
@@ -3,36 +3,33 @@ const moment = require("moment");
 const { calculateRemainingDays } = require("./calculateRemainingDays");
 
 
-const storyConvertor = (issue, sprints) => {
+const storyConvertor = (issue, dataForProjectLead) => {
     const getDate = (date) => moment(date).format("MMM Do YYYY, h:mm:ss A");
-    if (issue.fields.issuetype.name === "Story") {
-        
-    }
     // Filtering issuelinks for 'blocked by' and 'blocks'
     const blockedBy = issue?.fields?.issuelinks
-        ?.filter(link => link.type.inward === "is blocked by")
+        ?.filter(link => link?.type?.inward === "is blocked by")
         ?.map(link => ({
-            id: link.inwardIssue.id,
-            key: link.inwardIssue.key,
-            summary: link.inwardIssue.fields.summary,
-            status: link.inwardIssue.fields.status.name,
-            priority: link.inwardIssue.fields.priority.name,
-            type: link.inwardIssue.fields.issuetype.name
+            id: link?.inwardIssue?.id,
+            key: link?.inwardIssue?.key,
+            summary: link?.inwardIssue?.fields?.summary,
+            status: link?.inwardIssue?.fields?.status?.name,
+            priority: link?.inwardIssue?.fields?.priority?.name,
+            type: link?.inwardIssue?.fields?.issuetype?.name
         })) || [];
 
     const blocks = issue?.fields?.issuelinks
         ?.filter(link => link.type.outward === "blocks")
         ?.map(link => ({
-            id: link.outwardIssue.id,
-            key: link.outwardIssue.key,
-            summary: link.outwardIssue.fields.summary,
-            status: link.outwardIssue.fields.status.name,
-            priority: link.outwardIssue.fields.priority.name,
-            type: link.outwardIssue.fields.issuetype.name
+            id: link?.outwardIssue?.id,
+            key: link?.outwardIssue?.key,
+            summary: link?.outwardIssue?.fields?.summary,
+            status: link?.outwardIssue?.fields?.status?.name,
+            priority: link?.outwardIssue?.fields?.priority?.name,
+            type: link?.outwardIssue?.fields?.issuetype?.name
         })) || [];
 
     const story = {
-        story_id: issue.id,
+        story_id: issue?.id,
         story_key: issue?.key,
         story_name: issue?.fields?.summary,
         story_type: issue?.fields?.issuetype?.name,
@@ -43,33 +40,35 @@ const storyConvertor = (issue, sprints) => {
             project_name: issue?.fields?.project?.name,
             project_key: issue?.fields?.project?.key,
             projectImage: issue?.fields?.project?.avatarUrls["32x32"],
+            project_lead:  dataForProjectLead ? (dataForProjectLead?.lead.displayName ? dataForProjectLead.lead.displayName : "") : null,
         },
         blockedBy: blockedBy,
         blocks: blocks,
         status_name: issue?.fields?.status?.name,
-        sprint_id: issue.fields.customfield_10018?.[0]?.id,
-        sprint_state: issue.fields.customfield_10018?.[0]?.state,
-        sprint_name: issue.fields.customfield_10018?.[0]?.name,
-        sprint_start: getDate(issue.fields.customfield_10018?.startDate),
-        sprint_end: getDate(issue.fields.customfield_10018?.endDate),
+        sprint_id: issue?.fields?.customfield_10018?.[0]?.id,
+        sprint_state: issue?.fields?.customfield_10018?.[0]?.state,
+        sprint_name: issue?.fields?.customfield_10018?.[0]?.name,
+        sprint_start: getDate(issue?.fields?.customfield_10018?.startDate),
+        sprint_end: getDate(issue?.fields?.customfield_10018?.endDate),
         story_ac_hygiene: issue?.fields?.customfield_10156 !== null ? "YES" : "NO",
         original_estimate: getDate(issue?.fields?.timetracking?.originalEstimate) || "Not added",
         remaining_estimate: getDate(issue?.fields?.timetracking?.remainingEstimate) || "Not added",
         time_spent: issue?.fields?.timetracking?.timeSpent || "Not added",
         story_reviewers: issue?.fields?.customfield_10003
-            ? issue.fields.customfield_10003.length !== 0
-                ? issue.fields.customfield_10003.map((r) => r.displayName).join(", ")
+            ? issue?.fields?.customfield_10003?.length !== 0
+                ? issue?.fields?.customfield_10003?.map((r) => r.displayName).join(", ")
                 : "Reviewers not added"
             : "Reviewers not added",
-        story_points: issue?.fields?.customfield_10020 == null ? 0 : issue.fields.customfield_10020,
+        //customfield_10026  for story points estimate
+        story_points: issue?.fields?.customfield_10020 ?? issue?.fields?.customfield_10026 ?? 0,
         updated: getDate(issue?.fields?.updated),
         creator: issue?.fields?.creator?.displayName,
-        assignee: issue?.fields?.assignee !== null ? issue.fields.assignee.displayName : "Not added",
+        assignee: issue?.fields?.assignee !== null ? issue?.fields?.assignee?.displayName : "Not added",
         email: issue?.fields?.assignee?.emailAddress,
-        duedate: getDate(issue?.fields?.duedate == null ? issue?.fields?.customfield_10018?.[issue.fields.customfield_10018.length - 1]?.endDate : issue.fields.duedate),
+        duedate: getDate(issue?.fields?.duedate == null ? issue?.fields?.customfield_10018?.[issue?.fields?.customfield_10018?.length - 1]?.endDate : issue?.fields?.duedate),
         // sprintDuration: sprintDuration,
         // daysSpent: daysSpent,
-        daysLeft: calculateRemainingDays(issue.fields.customfield_10018?.endDate),
+        daysLeft: calculateRemainingDays(issue?.fields?.customfield_10018?.endDate),
         number_of_sub_tasks: issue?.fields?.subtasks?.length,
         completed_sub_tasks: issue?.fields?.subtasks?.filter(subtask => subtask?.fields?.status?.name === "Done")?.length,
         subtasks: issue?.fields?.subtasks?.map(subtask => {
