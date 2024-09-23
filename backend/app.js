9d6eec11545bffbaff39527a60237e04ebd115bb
jatin-gaian <jatin.s@gaiansolutions.com>
2024-09-23 17:14:59 +0530

    conflicts resolved

--- backend/app.js
+++ backend/app.js
@@ -2389,5 +2389,5 @@ app.post("/mib/webhook", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  // // conole.log(`Server running on port ${PORT}...`);
+  console.log(`Server running on port ${PORT}...`);
 });
